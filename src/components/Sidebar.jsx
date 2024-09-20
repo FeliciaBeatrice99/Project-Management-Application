e46a1557006c231cb28663905b43621ea14081cb
Felicia Beatrice <116735069+FeliciaBeatrice99@users.noreply.github.com>
2024-09-20 02:31:33 +0000

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button.jsx"
 
-export default function Sidebar() {
+export default function Sidebar({onStartAddProject}) {
     return <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">YOUR PROJECTS</h2>
         <div>
-            <Button text="+ ADD Project" />
+            <Button onClick={onStartAddProject}>+ ADD Project</Button>
 
         </div>
     </aside>
