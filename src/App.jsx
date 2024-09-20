22bfd35e7af47e07d34c4398adae32373b15d277
Felicia Beatrice <116735069+FeliciaBeatrice99@users.noreply.github.com>
2024-09-20 02:31:17 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import Sidebar from "./components/Sidebar";
 import MainDisplay from "./components/MainDisplay";
 import Project from "./components/Project";
+import NoProject from "./components/NoProject";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <Project />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProject onStartAddProject={handleStartAddProject} />;
+  }
   return (
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <Project />
+        <Sidebar  onStartAddProject={handleStartAddProject}/>
+        {content}
       </main>
   );
 }
