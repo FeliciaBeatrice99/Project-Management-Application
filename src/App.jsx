f03b7eae39875c28d74c877d145dc14fdf8c746b
Felicia Beatrice <116735069+FeliciaBeatrice99@users.noreply.github.com>
2024-09-24 06:29:59 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,40 @@ function App() {
     });
   }
 
+  function handleSaveProject(projectData) {
+    const projectId = Math.random();
+    const newProject = {
+      ...projectData,
+      id: projectId
+    }
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects,newProject],
+      };
+    });
+  }
+  
+  function handleCancel() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+  console.log(projectsState)
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <Project />
+    content = <Project onAdd={handleSaveProject} onCancel={handleCancel}/>
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProject onStartAddProject={handleStartAddProject} />;
   }
   return (
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar  onStartAddProject={handleStartAddProject}/>
+        <Sidebar  onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
         {content}
       </main>
   );
